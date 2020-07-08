9c0b1a326ca8453df60f824cf476f12318171e35
Alexander De Michieli <ademichieli@squarespace.com>
2020-07-08 14:52:51 -0700

    saving

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const expressServer = app.listen(PORT, '0.0.0.0', () => {
     console.log('Server is running s on port: ' + PORT)
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
