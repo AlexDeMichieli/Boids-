850ee0de756315de8203269f4371d0d818f1052f
Alexander De Michieli <ademichieli@squarespace.com>
2020-07-07 23:15:49 -0700

    boilderplate added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const router = express.Router();
+
+const PORT = process.env.PORT || 9000 
+const expressServer = app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server is running s on port: ' + PORT)
+});
+
+app.use(express.static('public'));
