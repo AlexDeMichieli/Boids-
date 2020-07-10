b2f2f4e44dc0c3dc7c13abb408b3d39c99055fa9
Alexander De Michieli <ademichieli@squarespace.com>
2020-07-09 21:41:59 -0700

    added triangles

--- public/Boid.js
+++ public/Boid.js
@@ -1,8 +1,9 @@
 class Boid{
     constructor(){
     
+    this.r = 2.0
     //Defines initial positon of the boid
-    this.position = createVector(Math.floor(Math.random(0, windowWidth) * 1000), Math.floor(Math.random(0, windowHeight) * 1000))
+    this.position = createVector(Math.floor(Math.random(0, windowWidth/2) * 1000), Math.floor(Math.random(0, windowHeight/2) * 1000))
     //this creates an initial circle
     // this.position = createVector(windowWidth/2, windowHeight/2)
     this.velocity = p5.Vector.random2D()
@@ -16,14 +17,22 @@ update(){
 }
 
 //1st of 3 important key rules. Alignment. 
+//the boid alignes
 align(){
 
 }
-    //draws the flock inside the canvas
+    //draws the flock inside the canvas. TO STYLE
     show(){
-        strokeWeight(16)
+        strokeWeight(4)
         stroke(255);
-        point(this.position.x, this.position.y);
+        // point(this.position.x, this.position.y);
+        // triangle(this.position.x1, this.position.y1, 20, 30, 40, 20 )
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(this.velocity.heading());
+        triangle(4, 0, -4, 3, -4, -3);
+        pop();
+    
     }
 }
 
